2d65e986315d49bbf159545513266eacb8ec96ab
Owen Stafford <owenstafford21@gmail.com>
2024-10-10 16:47:08 -0400

    setup_1

---
+++ qp.ts
@@ -0,0 +1,18 @@
+// qp.js
+const { execSync } = require('child_process');
+
+const commitMessage = process.argv[2];
+
+if (!commitMessage) {
+	console.error('Commit message is required.');
+	process.exit(1);
+}
+
+try {
+	execSync('git add .', { stdio: 'inherit' });
+	execSync(`git commit -m "${commitMessage}"`, { stdio: 'inherit' });
+	execSync('git push', { stdio: 'inherit' });
+} catch (error) {
+	console.error('Error executing git commands', error);
+	process.exit(1);
+}
